4afe6d41d0341ab58d70a28b5198e0beabaafeaf
Han <jinhan9657@naver.com>
2025-06-09 09:15:57 +0900

    250602 <07.서블릿 비즈니스 로직 처리>

---
+++ pro03/src/main/java/sec01/ex02/MemberDAO.java
@@ -0,0 +1,66 @@
+package sec01.ex02;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import sec01.ex02.MemberVO;
+
+public class MemberDAO {
+	private Connection con;
+	private PreparedStatement pstmt;
+	
+	private final String driver = "oracle.jdbc.driver.OracleDriver";
+    private final String url = "jdbc:oracle:thin:@localhost:1521:testdb"; // 환경에 맞게 수정
+    private final String user = "scott"; // 본인 계정
+    private final String pwd = "1234";  // 본인 비밀번호
+	
+	public List listMembers() {
+		List list = new ArrayList();
+		try
+		{
+			connDB();
+			String query = "select * from t_member";
+			System.out.println("prepareStatement: " + query);
+			pstmt = con.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery();
+		
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				MemberVO vo = new MemberVO();
+				vo.setId(id);
+				vo.setPwd(pwd);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setJoinDate(joinDate);
+				list.add(vo);
+			}
+			rs.close();
+			pstmt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	private void connDB() {
+		try {
+			Class.forName(driver);
+			System.out.println("Oracle 드라이버 로딩 성공");
+			con = DriverManager.getConnection(url, user, pwd);
+			System.out.println("Connection 생성 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
